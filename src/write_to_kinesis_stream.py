3fe6d10c33fee6360c836df194eda6d00cdf772a
mddarr <mddarr@gmail.com>
2020-11-15 00:23:28 -0800

    Defne a kinesis stream in CloudFormation

---
+++ src/write_to_kinesis_stream.py
@@ -0,0 +1,24 @@
+import boto3
+import json
+from datetime import datetime
+import calendar
+import random
+import time
+
+my_stream_name = 'python-stream'
+
+kinesis_client = boto3.client('kinesis', region_name='us-east-1')
+
+def put_to_stream(thing_id, property_value, property_timestamp):
+    payload = {
+                'prop': str(property_value),
+                'timestamp': str(property_timestamp),
+                'thing_id': thing_id
+              }
+
+    print(payload)
+
+    put_response = kinesis_client.put_record(
+                        StreamName=my_stream_name,
+                        Data=json.dumps(payload),
+                        PartitionKey=thing_id)
