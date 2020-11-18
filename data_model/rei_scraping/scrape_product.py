eb53437942980e307db5b3f0fa62f417ffdaf7c4
mddarr <mddarr@gmail.com>
2020-11-18 14:35:45 -0800

    Parse the name, colors & vendor of a product

--- data_model/rei_scraping/scrape_product.py
+++ data_model/rei_scraping/scrape_product.py
@@ -17,12 +17,38 @@ def get_product_detail(url):
     size_colors = size_list['colors']
 
     colors = {item['label'] for item in size_colors}
-    return colors
+
+    product_soup = soup.find("div", {"id": "product-container"})
+
+    vendor = product_soup.find('input', {'name': 'vendor'})['value']
+
+    product_name = product_soup.find('input', {'name': 'product_desc'})['value']
+    parsed_name = product_name.split(' - ')[0]
+    parsed_name = parsed_name.replace(vendor, '').lstrip()
+
+    product = {'name': parsed_name, 'vendor': vendor, 'colors': colors}
+    return product
 
 
+# url = 'https://www.rei.com/product/154144/patagonia-better-sweater-fleece-jacket-mens'
+# page = requests.get(url)
+# soup = BeautifulSoup(page.text, 'html.parser')
+# product_soup = soup.find("div", {"id": "product-container"})
+#
+# vendor = product_soup.find('input',{'name':'vendor'})['value']
+#
+# product_name = product_soup.find('input',{'name':'product_desc'})['value']
+# parsed_name = product_name.split(' - ')[0]
+# parsed_name = parsed_name.replace(vendor, '').lstrip()
+#
+#
+#
+# product_selection = soup.find("div", {"class": "product-color-and-size-selection"})
 
+
+#product-container > div:nth-child(2) > div > div.col-xs-12.col-md-8.product-media-wrapper.apparel-media-wrapper > div.product-title > h1
 
 
 url = 'https://www.rei.com/product/154144/patagonia-better-sweater-fleece-jacket-mens'
 
-colors = get_product_detail(url)
+product = get_product_detail(url)
