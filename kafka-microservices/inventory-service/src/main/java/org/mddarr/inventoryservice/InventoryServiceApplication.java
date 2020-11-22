fd56468f61b1f25332ce88680a10d297113bdaf3
mddarr <mddarr@gmail.com>
2020-11-22 12:19:38 -0800

    Add the store front end

--- kafka-microservices/inventory-service/src/main/java/org/mddarr/inventoryservice/InventoryServiceApplication.java
+++ kafka-microservices/inventory-service/src/main/java/org/mddarr/inventoryservice/InventoryServiceApplication.java
@@ -3,6 +3,10 @@ package org.mddarr.inventoryservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+
+import java.util.function.Consumer;
+import java.util.function.Supplier;
 
 @SpringBootApplication
 @EnableCaching
@@ -10,6 +14,51 @@ public class InventoryServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(InventoryServiceApplication.class, args);
+	}
+	@Bean
+	public Consumer<String> sink1() {
+		return message -> {
+			System.out.println("******************");
+			System.out.println("At Sink1");
+			System.out.println("******************");
+			System.out.println("Received message " + message);
+		};
+	}
+
+	@Bean
+	public Consumer<String> sink2() {
+		return message -> {
+			System.out.println("******************");
+			System.out.println("At Sink2");
+			System.out.println("******************");
+			System.out.println("Received message " + message);
+		};
+	}
+
+	@Bean
+	public Supplier<String> source1() {
+		return () -> {
+			String message = "FromSource1";
+			System.out.println("******************");
+			System.out.println("From Source1");
+			System.out.println("******************");
+			System.out.println("Sending value: " + message);
+			return message;
+
+		};
+	}
+
+	@Bean
+	public Supplier<String> source2() {
+		return () -> {
+			String message = "FromSource2";
+			System.out.println("******************");
+			System.out.println("From Source2");
+			System.out.println("******************");
+			System.out.println("Sending value: " + message);
+			return message;
+
+		};
 	}
 
 }
--- kafka-microservices/inventory-service/src/main/java/org/mddarr/inventoryservice/processors/BranchingProcessor.java
+++ kafka-microservices/inventory-service/src/main/java/org/mddarr/inventoryservice/processors/BranchingProcessor.java
@@ -20,26 +20,26 @@ import java.util.Date;
 public class BranchingProcessor {
 //    @EnableBinding({WordBranches.class})
 //    @EnableAutoConfiguration
-    public static class WordCountProcessorApplication {
-        @StreamListener("words")
-        @SendTo({"english", "french", "spanish"})
-        @SuppressWarnings("unchecked")
-        public KStream<?, WordCount>[]process(KStream < Object, String>input) {
-
-            Predicate<Object, WordCount> isEnglish = (k, v) -> v.word.equals("english");
-            Predicate<Object, WordCount> isFrench = (k, v) -> v.word.equals("french");
-            Predicate<Object, WordCount> isSpanish = (k, v) -> v.word.equals("spanish");
-
-            return input
-                    .flatMapValues(value -> Arrays.asList(value.toLowerCase().split("\\W+")))
-                    .groupBy((key, value) -> value)
-                    .windowedBy(TimeWindows.of(Duration.ofSeconds(6)))
-                    .count(Materialized.as("WordCounts-1"))
-                    .toStream()
-                    .map((key, value) -> new KeyValue<>(null, new WordCount(key.key(), value, new Date(key.window().start()), new Date(key.window().end()))))
-                    .branch(isEnglish, isFrench, isSpanish);
-        }
-    }
+//    public static class WordCountProcessorApplication {
+//        @StreamListener("words")
+//        @SendTo({"english", "french", "spanish"})
+//        @SuppressWarnings("unchecked")
+//        public KStream<?, WordCount>[]process(KStream < Object, String>input) {
+//
+//            Predicate<Object, WordCount> isEnglish = (k, v) -> v.word.equals("english");
+//            Predicate<Object, WordCount> isFrench = (k, v) -> v.word.equals("french");
+//            Predicate<Object, WordCount> isSpanish = (k, v) -> v.word.equals("spanish");
+//
+//            return input
+//                    .flatMapValues(value -> Arrays.asList(value.toLowerCase().split("\\W+")))
+//                    .groupBy((key, value) -> value)
+//                    .windowedBy(TimeWindows.of(Duration.ofSeconds(6)))
+//                    .count(Materialized.as("WordCounts-1"))
+//                    .toStream()
+//                    .map((key, value) -> new KeyValue<>(null, new WordCount(key.key(), value, new Date(key.window().start()), new Date(key.window().end()))))
+//                    .branch(isEnglish, isFrench, isSpanish);
+//        }
+//    }
 
 //    interface WordBranches {
 //        @Input("words")
@@ -54,42 +54,42 @@ public class BranchingProcessor {
 
 
 
-
-    static class WordCount {
-        private String word;
-        private long count;
-        private Date start;
-        private Date end;
-        WordCount(String word, long count, Date start, Date end) {
-            this.word = word;
-            this.count = count;
-            this.start = start;
-            this.end = end;
-        }
-        public String getWord() {
-            return word;
-        }
-        public void setWord(String word) {
-            this.word = word;
-        }
-        public long getCount() {
-            return count;
-        }
-        public void setCount(long count) {
-            this.count = count;
-        }
-        public Date getStart() {
-            return start;
-        }
-        public void setStart(Date start) {
-            this.start = start;
-        }
-        public Date getEnd() {
-            return end;
-        }
-        public void setEnd(Date end) {
-            this.end = end;
-        }
-    }
+//
+//    static class WordCount {
+//        private String word;
+//        private long count;
+//        private Date start;
+//        private Date end;
+//        WordCount(String word, long count, Date start, Date end) {
+//            this.word = word;
+//            this.count = count;
+//            this.start = start;
+//            this.end = end;
+//        }
+//        public String getWord() {
+//            return word;
+//        }
+//        public void setWord(String word) {
+//            this.word = word;
+//        }
+//        public long getCount() {
+//            return count;
+//        }
+//        public void setCount(long count) {
+//            this.count = count;
+//        }
+//        public Date getStart() {
+//            return start;
+//        }
+//        public void setStart(Date start) {
+//            this.start = start;
+//        }
+//        public Date getEnd() {
+//            return end;
+//        }
+//        public void setEnd(Date end) {
+//            this.end = end;
+//        }
+//    }
 
 }
