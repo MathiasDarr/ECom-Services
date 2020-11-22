0273a01c82ff634205e1f7a08e7d32acddddc0c0
mddarr <mddarr@gmail.com>
2020-11-21 21:58:48 -0800

    Getting this to compile has been a nightmare!

--- kafka-microservices/producers/src/main/java/org/mddarr/producers/ProductAvroProducer.java
+++ kafka-microservices/producers/src/main/java/org/mddarr/producers/ProductAvroProducer.java
@@ -3,13 +3,15 @@ package org.mddarr.producers;
 import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
-import com.amazonaws.services.dynamodbv2.document.DynamoDB;
-import com.amazonaws.services.dynamodbv2.document.Table;
+import com.amazonaws.services.dynamodbv2.document.*;
+import com.amazonaws.services.dynamodbv2.model.AttributeValue;
+import com.amazonaws.services.dynamodbv2.model.ScanRequest;
+import com.amazonaws.services.dynamodbv2.model.ScanResult;
 import io.confluent.kafka.serializers.AbstractKafkaAvroSerDeConfig;
 import io.confluent.kafka.serializers.KafkaAvroSerializer;
 import io.confluent.kafka.streams.serdes.avro.SpecificAvroSerializer;
 import org.apache.kafka.clients.producer.ProducerConfig;
-import org.mddarr.producers.processors.InventoryProcessor;
+
 import org.mddarr.products.AvroProduct;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
@@ -19,14 +21,37 @@ import java.io.FileReader;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.Statement;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
+import java.util.*;
 
 public class ProductAvroProducer {
+
+    static AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard().build();
+    static DynamoDB dynamoDB = new DynamoDB(client);
+    static String tableName = "Products";
+
     public static void main(String[] args) {
         populateProductsKafkaTopic();
+    }
+
+
+    private static void scanProductsTable() {
+
+        Table table = dynamoDB.getTable(tableName);
+
+//        Map<String, Object> expressionAttributeValues = new HashMap<String, Object>();
+//        expressionAttributeValues.put(":pr", 100);
+
+        ItemCollection<ScanOutcome> items = table.scan(); //"price < 200", // FilterExpression
+//                "Id, Title, ProductCategory, Price", // ProjectionExpression
+//                null, // ExpressionAttributeNames - not used in this example
+//                expressionAttributeValues);
+
+        System.out.println("Scan of " + tableName );
+
+        for (Item item : items) {
+            System.out.println(item.toJSONPretty());
+        }
+
     }
 
     public static DefaultKafkaProducerFactory<String, AvroProduct> getKafkaProducerFactory(){
@@ -54,17 +79,31 @@ public class ProductAvroProducer {
         return pf1;
     }
 
+    public static DynamoDB getDynamoDB(){
+        AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard()
+                .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration("http://localhost:4566", "us-west-2"))
+                .build();
+        return new DynamoDB(client);
+    }
+
     public static void populateProductsKafkaTopic(){
 
         DefaultKafkaProducerFactory<String, AvroProduct> pf1 = getKafkaProducerFactory();
         KafkaTemplate<String, AvroProduct> template1 = new KafkaTemplate<>(pf1, true);
-        template1.setDefaultTopic(InventoryProcessor.PRODUCT_FEED);
+        template1.setDefaultTopic("Products-Kafka-Topic");
 
-        AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard()
-                .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration("http://localhost:8000", "us-west-2"))
-                .build();
-        DynamoDB dynamoDB = new DynamoDB(client);
-        Table table = dynamoDB.getTable("Movies");
+        scanProductsTable();
+
+//        AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard().build();
+//
+//        ScanRequest scanRequest = new ScanRequest()
+//                .withTableName("Reply");
+//
+//        ScanResult result = client.scan(scanRequest);
+//        for (Map<String, AttributeValue> item : result.getItems()){
+//            printItem(item);
+//        }
+
 
 
     }
